92a7d056f109c1529cccf2ea5408352766f4c56f
ming-der wang <mingderwang@me.com>
2025-04-19 22:07:25 +0800

    init

---
+++ README.md
@@ -0,0 +1,40 @@
+# yes just elysia
+
+## login
+
+```
+ming-ders-MacBook.local💩➜  ~ curl http://localhost:8080/login \
+  --request POST \
+  --header 'Content-Type: application/json' \
+  --data '{
+  "username": "test",
+  "password": "password"
+}'
+{"success":true,"token":"eyJhbGciOiJIUzI1NiJ9.eyJpZCI6InRlc3QifQ.a2Oyv6Kgxv_Q9JPr3rn0c7ECnzk5aob9GK3onOZKZuw"}
+```
+
+## sign
+
+```
+ming-ders-MacBook.local💩➜  ~ curl -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6InRlc3QifQ.a2Oyv6Kgxv_Q9JPr3rn0c7ECnzk5aob9GK3onOZKZuw" http://localhost:8080/sign
+{"id":"test"}
+
+ming-ders-MacBook.local💩➜  ~ curl -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6InRlc3QifQ.a2Oyv6Kgxv_Q9JPr3rn0c7ECnzk5aob9GK3onOZKZu" http://localhost:8080/sign
+Invalid token
+```
+
+# build
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.ts
+```
+
+This project was created using `bun init` in bun v1.1.20. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ index.ts
@@ -0,0 +1,45 @@
+import { Elysia, t} from 'elysia'
+import { bearer } from '@elysiajs/bearer'
+import { jwt } from '@elysiajs/jwt'
+import { swagger } from '@elysiajs/swagger'
+
+const app = new Elysia()
+    .use(swagger())
+    .use(bearer())
+    .use(jwt({
+        name: 'jwt',
+        secret: 'your-secret-key'
+    }))
+  // Login endpoint to generate a token
+  .post(
+    '/login',
+    async ({ body, jwt }) => {
+      const { username, password } = body;
+      // Replace this with actual user validation (e.g., check against a database)
+      if (username === 'test' && password === 'password') {
+        const token = await jwt.sign({ id: username });
+        return { success: true, token };
+      }
+      throw new Error('Invalid credentials');
+    },
+    {
+      body: t.Object({
+        username: t.String(),
+        password: t.String(),
+      }),
+    }
+  )
+.get('/sign', async ({ bearer, jwt }) => {
+        const payload = await jwt.verify(bearer)
+        if (!payload) return 'Invalid token'
+        return payload
+    }, {
+        beforeHandle({ bearer, set }) {
+            if (!bearer) {
+                set.status = 400
+                set.headers['WWW-Authenticate'] = `Bearer realm='sign', error="invalid_request"`
+                return 'Unauthorized'
+            }
+        }
+    })
+    .listen(8080)
